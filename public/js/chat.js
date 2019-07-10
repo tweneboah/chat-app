55507446380cb1b964ead40224310acbea466b69
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-07-10 14:53:50 +0800

    socket events

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,22 @@
 //This is function that shows connected user because we embed client side script into our html
-io()
 
+//This is where we call the functions on main file
+
+//This file is the client and the main file is the server
+
+const socket = io();
+//We have to receive the event the server is returning to us
+
+//All the event created by our sever is are store inside socket variab;e
+
+
+socket.on('countUpdated', (count) => {
+ //The callback receive all functions/varriables pass to it by emit event
+ console.log('The count has updated', count)
+});
+
+document.querySelector('#increment').addEventListener('click', () => {
+ console.log('Click')
+ socket.emit('increment')
+})
 
--- src/app.js
+++ src/app.js
@@ -12,18 +12,35 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 
-
-
-
 //Serving static files
-const publicDirectoryPath = path.join(__dirname, './public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 //check if there is new user connected to our sever.
 //To see the new user connected we have to connect it to our client side of the application
-io.on('connection', () => {
-   console.log('New websocket connection')
+
+//Creating a count that will show to every user connected
+//io.on accept a value call socket where we can have access to many methods eg to show connected user
+
+//If there is 5 new user connected, this function will call 5 times
+
+let count = 0;
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection');
+//We are creating all the events here then we will receive it on chat.js file
+
+//socket.emit acc
+socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+       count++
+       socket.emit('countUpdated', count);
+       io.emit('countUpdated', count) //Live updating
+    })
 })
+
+
 
 //SERVER
 const port  = process.env.PORT || 5000;
